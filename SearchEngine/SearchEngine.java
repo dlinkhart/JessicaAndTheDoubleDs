7bd21aa380f058f787239ff6ee44c5d86ea82967
JessicaSandbox <BryantJessica@outlook.com>
2017-01-28 15:38:37 -0500

    Initial Commit from Eclipse ~~Jessica
    I will rename this repo and package when we come up with a team name.
    I just wanted to give us a jumping off point on this project.
    I suggest just copy and past the code into your ide of choice if you
    want to play around with it until i rename everything; then you can push
    to the updated repo without having to change any settings.

---
+++ SearchEngine/SearchEngine.java
@@ -0,0 +1,24 @@
+/*Created by Jessica Stepp, Douglas *, and Douglas * JavaII 2017
+ * Java II project Search Engine: creating a GUI file search
+ * that includes file upload and delete tools.
+ */
+package SearchEngine;// I will rename the package when we come up
+                     // with an awesome team name :)
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class SearchEngine {
+
+	public static void main(String[] args) {
+		//open search window
+		JFrame frame = new JFrame("Search Engine");
+		frame.setSize(800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}//end main
+
+}//end class SearchEngine
